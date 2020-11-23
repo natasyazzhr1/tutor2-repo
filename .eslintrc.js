db7a1ec7c2b72146f18ecb51b934aaba7471dc8c
Faris Rizki Ekananda <13518125@std.stei.itb.ac.id>
2020-11-23 18:30:19 +0700

    configure eslint & deleting vue template style

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,26 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    es6: true,
+    jest: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
+    ecmaVersion: 2018,
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    "eslint:recommended",
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // Pertimbangkan untuk menggunakan `plugin:vue/strongly-recommended` atau `plugin:vue/recommended` untuk memperketat aturan.
+    "plugin:vue/recommended",
   ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+  // diperlukan untuk melinting file *.vue
+  plugins: ["vue"],
+  // tambahkan aturan anda disini
+  rules: {
+    semi: [2, "never"],
+    "no-console": "off",
+    "vue/max-attributes-per-line": "off",
+  },
 }
