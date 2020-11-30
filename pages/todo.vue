eab8ebf3aaaad27b7bda27f25e3e097dd141d3c2
Faris Rizki Ekananda <13518125@std.stei.itb.ac.id>
2020-11-30 18:58:15 +0700

    change default vuetify style

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    ini profile page kmu
+    ini todo
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-}
+  export default {
+    name: 'Todo'
+  }
 </script>
 
 <style lang="scss" scoped>
